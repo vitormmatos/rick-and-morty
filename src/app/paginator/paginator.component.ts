c54d3ba757cdad8d6bb874ddb1d530860930bbfe
vitormmatos <vitor.matos@criptext.com>
2022-07-26 09:26:55 -0300

    ACTIVE PAGE NOW SHOWS ON PAGINATOR
    stylistic changes as well

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -6,7 +6,8 @@ import { Router } from '@angular/router'
   templateUrl: './paginator.component.html'
 })
 export class PaginatorComponent {
-  @Input() pageName: string = ''
+  @Input() pageName!: string
+  @Input() pageNumber!: string
 
   constructor (private readonly router: Router) {}
 
@@ -15,5 +16,13 @@ export class PaginatorComponent {
     setTimeout(() => {
       window.location.reload()
     }, 10)
+  }
+
+  activeButton(index: number): string{
+    if(this.pageNumber === index.toString()){
+      return "btn-active"
+    }
+
+    return ""
   }
 }
